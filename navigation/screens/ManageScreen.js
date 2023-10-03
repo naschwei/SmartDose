a5d14c352f2953cc8fe4d9fc084064fbe52d6721
Apurva <apurvad@umich.edu>
2023-10-03 10:57:00 -0400

    imagetotextAPI function added

--- navigation/screens/ManageScreen.js
+++ navigation/screens/ManageScreen.js
@@ -11,3 +11,55 @@ export default function ManageScreen({ navigation }) {
         </View>
     );
 }
+
+async function extractMedicineName(imageData, imageToTextApiKey) {
+    //  Upload the image to Image to Text API
+    const imageToTextUrl = 'https://api.api-ninjas.com/v1/imagetotext';
+    const formData = new FormData();
+    formData.append('image', imageData);
+  
+    const imageToTextConfig = {
+      method: 'POST',
+      headers: {
+        'X-Api-Key': imageToTextApiKey,
+      },
+      body: formData,
+    };
+  
+    const response = await fetch(imageToTextUrl, imageToTextConfig);
+    const textData = await response.json();
+    // Extract text
+    const extractedText = textData
+        .filter(item => item.text) // Filter out empty text
+        .map(item_1 => item_1.text.trim()) // Trim whitespace
+        .join(' ');
+    // Search for extracted text in Medicine Name Search API
+    const medicineNameSearchUrl = `https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?terms=${encodeURIComponent(
+        extractedText
+    )}`;
+    const response_1 = await fetch(medicineNameSearchUrl);
+    const medicineData = await response_1.json();
+    // Extract medicine names from Name Search API response ()
+    if (medicineData.results && medicineData.results.length > 0) {
+        const medicineNames = medicineData.results.map(result_1 => result_1.DISPLAY_NAME);
+        return medicineNames;
+    } else {
+        return null; // No matching names found
+    }
+  }
+  
+  // Example (need to add image upload functionality):
+  // const imageFile = document.getElementById('yourImageInput').files[0];
+  const imageToTextApiKey = 'yqfk5vqF5e/nVAhECxZgRw==1JSnGDvmDsKjxB3t';
+  
+//   extractMedicineName(imageFile, imageToTextApiKey)
+//     .then(medicineNames => {
+//       if (medicineNames) {
+//         console.log('Matching Medicine Names:', medicineNames);
+//       } else {
+//         console.log('No matching medicine names found.');
+//       }
+//     })
+//     .catch(error => {
+//       console.error('Error:', error);
+//     });
