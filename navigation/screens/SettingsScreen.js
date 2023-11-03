ed8ec0391849632456b527ef46f1a78f3fdb07f6
naschwei <naschwei@umich.edu>
2023-11-03 12:59:49 -0400

    manage medication camera

--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import Modal from 'react-native-modal';
+import {useState} from 'react';
 
 export default function SettingsScreen({ navigation }) {
+    const [ isModalVisible, setIsModalVisible ] = useState(false);
+
+    
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold'}}>Settings Screen</Text>
+        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+            
+
+
         </View>
     );
 }
